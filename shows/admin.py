e7c94203c55d6916db128f3b62320ec71146b2c8
Tim Stumbaugh <stum@mit.edu>
2013-01-20 21:51:02 -0500

    Implement inline editing of a show's dances

--- shows/admin.py
+++ shows/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from dt.shows.models import *
 
+class DanceInline(admin.TabularInline):
+    model = Dance
+    exclude = ('dancers',)
+
 class ShowAdmin(admin.ModelAdmin):
     list_display = ('name', 'year', 'semester')
+    inlines = (DanceInline,)
 
 class DanceAdmin(admin.ModelAdmin):
     list_display = ('name', 'show', 'style', 'level')
     filter_horizontal = ('choreographers', 'dancers')
-
 
 admin.site.register(Show, ShowAdmin)
 admin.site.register(Dance, DanceAdmin)
