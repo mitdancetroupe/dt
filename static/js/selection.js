bff2c584d244291e3e0610c17f3b11ae373ddd51
bfaviero <bfaviero@mit.edu>
2014-01-30 14:15:54 -0500

    Works with new object type

--- static/js/selection.js
+++ static/js/selection.js
@@ -1,26 +1,27 @@
 var prefs = null;
-
-function setPrefs(response){
-    prefs = eval(response);
-}
+var dancers = null;
 
 $.ajax({
 	async: false,
 	url: "http://127.0.0.1:8000/auditions/S14/selection_prefsheets/2",
 	success: function(response){
-        setPrefs(response);
+		response = JSON.parse(response);
+        dancers = response.dancers;
+        prefs = response.prefs;
+        load_prefs();
     }
 });
-
-$.each(prefs, function(i, pref) {
-	var source   = $("#profile-template").html();
-	var template = Handlebars.compile(source);
-	var name = pref.user.first_name+" "+pref.user.last_name;
-	var conflicts = pref.prefsheet.conflicts;
-	var context = {name: name, conflicts: conflicts}
-	var html = template(context);
-	$( "#center-column" ).append(html);
-});
+function load_prefs() {
+	$.each(prefs, function(i, pref) {
+		var source   = $("#profile-template").html();
+		var template = Handlebars.compile(source);
+		var name = pref.user.first_name+" "+pref.user.last_name;
+		var conflicts = pref.prefsheet.conflicts;
+		var context = {name: name, conflicts: conflicts}
+		var html = template(context);
+		$( "#center-column" ).append(html);
+	});
+}
 
 
 
