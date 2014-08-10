64cec552ed722ba6ef97d41bbdc34b613bff4462
Bruno Faviero <bruno@kensho.com>
2014-08-10 15:25:10 -0400

    Delete unused JS files

--- static/js/prefsheet.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-    var addPref = function(prefTable) {
-        totalForms = $("#id_prefs-TOTAL_FORMS", prefTable).val();
-        $("#id_prefs-TOTAL_FORMS", prefTable).val(totalForms++);
-    }; 
-})(jQuery);
--- static/js/render_header.js
+++
@@ -1,4 +0,0 @@
-var head_text = document.createElement('h1');
-var text = c_main.get_name() + ' Dancer Selection';
-head_text.innerHTML = text;
-document.getElementById('head').appendChild(head_text);
